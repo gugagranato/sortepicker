148fa206626055d513b29312e580641069b1a0f2
Gustavo Granato Silva <gustavo.granato@hotmail.com>
2019-10-25 14:09:03 -0300

    bugfix

--- src/Pages/Sugestion/index.js
+++ src/Pages/Sugestion/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Sugestion() {
-  return (
-    <h1>Sugestion</h1>
-  );
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Sugestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nomes: ['Guga'],
+      newName: ''
+    }
+  }
+
+  mude = n => {
+    this.setState({
+      newName: n.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      nomes: [...this.state.nomes, this.state.newName],
+      newName: ''
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.nomes.map(nome => <li key={nome}>{nome}</li> )}
+        </ul>
+        <input type='text' onChange={this.mude} value={this.state.newName}/>
+        <button type="submit">Enviar</button>
+      </form>
+    )
+  }
 }
